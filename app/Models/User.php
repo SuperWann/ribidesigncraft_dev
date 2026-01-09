1c0ef5d99d290d8e4d6eb98bd031d2a2f05c107f
Stwn25 <msetiawanwibisono@gmail.com>
2026-01-09 22:13:06 +0700

    feat: add filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -62,5 +63,11 @@ class User extends Authenticatable
     public function isCustomer()
     {
         return $this->role === 0;
+    }
+    
+    // ===== FILAMENT =====
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->isAdmin();
     }
 }
