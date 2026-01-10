2f595427d3acaff855ba5d4f658caea0c083c3e7
Stwn25 <msetiawanwibisono@gmail.com>
2026-01-10 15:18:18 +0700

    fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,20 @@
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/js/app.js')
-        @inertiaHead
-    </head>
-    <body>
-        @inertia
-    </body>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Montserrat -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    @viteReactRefresh
+    @vite('resources/js/app.tsx')
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
 </html>
