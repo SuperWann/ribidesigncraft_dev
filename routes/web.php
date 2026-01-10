aa7e9c6e4792fa12f79263dd41c630e81457dc7c
Stwn25 <msetiawanwibisono@gmail.com>
2026-01-10 15:16:59 +0700

    fix: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return Inertia::render('Beranda/Index');
+})->name('beranda');
+
+Route::get('/belanja', [ProductController::class, 'index'])->name('belanja');
+Route::get('/belanja/{product}', [ProductController::class, 'show'])->name('belanja.show');
+
+Route::get('/testimoni', function () {
+    return Inertia::render('Testimoni/Index');
+})->name('testimoni');
+
+Route::get('/wishlist', function () {
+    return Inertia::render('Wishlist/Index');
+})->name('wishlist');
