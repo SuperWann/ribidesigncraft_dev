6659c0940465ca458280f7dc46ee160a64558d91
Stwn25 <msetiawanwibisono@gmail.com>
2026-02-09 09:34:51 +0700

    fix: preview.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -23,4 +23,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'resources/js'),
         },
     },
+    preview: {
+        allowedHosts: 'ribidesigncraft-dev.onrender.com',
+    },
 });
